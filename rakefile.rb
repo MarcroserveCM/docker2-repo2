ac471e7b95ce941a5259c469e443c2568423720c
MarcroserveCM <49010421+MarcroserveCM@users.noreply.github.com>
2019-06-21 12:35:04 +0100

    Update rakefile.rb

--- rakefile.rb
+++ rakefile.rb
@@ -12,12 +12,4 @@ CLOBBER.include("**/*.#{$dlext}", "**/*~", "**/*#*", "**/*.log", "**/*.o")
 FileList["ext/**/extconf.rb", "ext/**/*.h", "ext/**/*.c", "examples/*.rb",
 "examples/media/*"].to_a 
 
-Rake::GemPackageTask.new(specification) do |package|
-    package.need_zip = false
-    package.need_tar = false
-end
-Rake::ExtensionTask.new('texplay', specification)  do |ext|
-#    ext.config_script = 'extconf.rb' 
-#    ext.cross_compile = true                
-#    ext.cross_platform = 'i386-mswin32'
-end
+
