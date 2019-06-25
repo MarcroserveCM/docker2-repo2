ae8d290861eaacfdd27fb2b07311020158d4a21e
MarcroserveCM <49010421+MarcroserveCM@users.noreply.github.com>
2019-06-25 11:02:53 +0100

    Update rakefile.rb

--- rakefile.rb
+++ rakefile.rb
@@ -1,16 +1,3 @@
-require "rails/all"
-require 'rake/clean'
 
-
-# get the texplay version
-
-$dlext = RbConfig::CONFIG['DLEXT']
-CLEAN.include("ext/**/*.#{$dlext}", "ext/**/*.log", "ext/**/*.o", "ext/**/*~", "ext/**/*#*", "ext/**/*.obj", "ext/**/*.def", "ext/**/*.pdb")
-CLOBBER.include("**/*.#{$dlext}", "**/*~", "**/*#*", "**/*.log", "**/*.o")
-
-   
-["lib/texplay.rb" "lib/texplay-contrib.rb" "lib/texplay/version.rb"] +
-FileList["ext/**/extconf.rb", "ext/**/*.h", "ext/**/*.c", "examples/*.rb",
-"examples/media/*"].to_a 
 
 
